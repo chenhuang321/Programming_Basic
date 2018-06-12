1ea97698826175f46adaff09b3ca61b72dd51648
Chen Huang <jameshuang7788@gmail.com>
2018-06-12 17:24:43 -0400

    Add files via upload

---
+++ react example/superstar/src/SuperStar.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./SuperStar.css"
+
+class SuperStar extends Component {
+    render() {
+        return (
+            <div className="starContainer">
+                <h1>Vote For Your Star</h1>
+            </div>
+        );
+    }
+}
+
+export default SuperStar;
---
+++ react example/superstar/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import SuperStar from "./SuperStar";
+import "./index.css"
+
+ReactDOM.render(
+    <SuperStar />,
+    document.getElementById("root")
+);
