3eff4ce22be05082acefe2f64d6764c167576a61
Chen Huang <jameshuang7788@gmail.com>
2018-08-08 16:53:13 -0400

    Add files via upload

---
+++ react example/book/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'JavaScript: The Good Parts', pages: 101 },
+    { title: 'Ethan Deng', pages: 39 },
+    { title: 'The Dark Basement', pages: 85 },
+    { title: 'Keith Sue Wong', pages: 1 }
+  ];
+}
